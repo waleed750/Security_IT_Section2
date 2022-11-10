b2041377406d22ba9f785b81dd0d50b38d06ac4c
waleed750 <102048956+waleed750@users.noreply.github.com>
2022-11-10 10:44:53 +0200

    Add files via upload

---
+++ MonoAlphabetic/MonoAlphabetic/Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace MonoAlphabetic
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.plainText = new System.Windows.Forms.TextBox();
+            this.result = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.randomBtn = new System.Windows.Forms.Button();
+            this.Encrypt = new System.Windows.Forms.Button();
+            this.decry = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // plainText
+            // 
+            this.plainText.Location = new System.Drawing.Point(111, 70);
+            this.plainText.Name = "plainText";
+            this.plainText.Size = new System.Drawing.Size(141, 20);
+            this.plainText.TabIndex = 0;
+            // 
+            // result
+            // 
+            this.result.Location = new System.Drawing.Point(111, 149);
+            this.result.Multiline = true;
+            this.result.Name = "result";
+            this.result.Size = new System.Drawing.Size(173, 67);
+            this.result.TabIndex = 1;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(111, 27);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(285, 20);
+            this.textBox3.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(38, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(25, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Key";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(38, 77);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(51, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "PlainText";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(29, 152);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(37, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Result";
+            // 
+            // randomBtn
+            // 
+            this.randomBtn.Location = new System.Drawing.Point(438, 27);
+            this.randomBtn.Name = "randomBtn";
+            this.randomBtn.Size = new System.Drawing.Size(156, 23);
+            this.randomBtn.TabIndex = 6;
+            this.randomBtn.Text = "Create Random Alhapet";
+            this.randomBtn.UseVisualStyleBackColor = true;
+            this.randomBtn.Click += new System.EventHandler(this.randomBtn_Click);
+            // 
+            // Encrypt
+            // 
+            this.Encrypt.Location = new System.Drawing.Point(267, 72);
+            this.Encrypt.Name = "Encrypt";
+            this.Encrypt.Size = new System.Drawing.Size(88, 23);
+            this.Encrypt.TabIndex = 7;
+            this.Encrypt.Text = "Encrypt";
+            this.Encrypt.UseVisualStyleBackColor = true;
+            this.Encrypt.Click += new System.EventHandler(this.Encrypt_Click);
+            // 
+            // decry
+            // 
+            this.decry.Location = new System.Drawing.Point(380, 72);
+            this.decry.Name = "decry";
+            this.decry.Size = new System.Drawing.Size(88, 23);
+            this.decry.TabIndex = 8;
+            this.decry.Text = "Decrypt";
+            this.decry.UseVisualStyleBackColor = true;
+            this.decry.Click += new System.EventHandler(this.decry_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(632, 385);
+            this.Controls.Add(this.decry);
+            this.Controls.Add(this.Encrypt);
+            this.Controls.Add(this.randomBtn);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.result);
+            this.Controls.Add(this.plainText);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox plainText;
+        private System.Windows.Forms.TextBox result;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button randomBtn;
+        private System.Windows.Forms.Button Encrypt;
+        private System.Windows.Forms.Button decry;
+    }
+}
+
---
+++ MonoAlphabetic/MonoAlphabetic/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MonoAlphabetic
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        private string randomAlhapet()
+        {
+            var chars = "abcdefghijklmnopqrstuvwxyz".ToUpper();
+            //var stringChars = new char[26];
+            var random = new Random();
+            
+            var stringChars = chars.OrderBy(x => random.Next()).ToArray();
+
+
+            return new String(stringChars);
+        }
+
+        private void randomBtn_Click(object sender, EventArgs e)
+        {
+            var newAlpha = randomAlhapet();
+            textBox3.Text = newAlpha;
+        }
+
+        private void Encrypt_Click(object sender, EventArgs e)
+        {
+            var newAplha = textBox3.Text;
+            var aplha = "abcdefghijklmnopqrstuvwxyz".ToUpper();
+            var plain = plainText.Text.ToUpper();
+            var encryptedText = "";
+
+            for (int i = 0; i < plain.Length; i++)
+            {
+                var index = aplha.IndexOf(plain[i]);
+
+                encryptedText += newAplha[index]; 
+            }
+            result.Text = encryptedText;
+        }
+
+        private void decry_Click(object sender, EventArgs e)
+        {
+            var newAplha = textBox3.Text;
+            var aplha = "abcdefghijklmnopqrstuvwxyz".ToUpper();
+            var plain = plainText.Text.ToUpper();
+            var decryptedText = "";
+            for (int i = 0; i < plain.Length; i++)
+            {
+                var index = newAplha.IndexOf(plain[i]);
+
+                decryptedText += aplha[index];
+            }
+            result.Text = decryptedText;
+        }
+    }
+}
